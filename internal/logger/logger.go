808b7c8c88a6db71fee778a3f38c7ce8ec00375e
Mohammad Taghipour <taghipourmohammad7@gmail.com>
2025-12-09 16:55:34 +0330

    🌱 logger

---
+++ internal/logger/logger.go
@@ -0,0 +1,24 @@
+package logger
+
+import (
+	"sync"
+
+	"go.uber.org/zap"
+)
+
+var (
+	l    *zap.SugaredLogger
+	once sync.Once
+)
+
+func Instance() *zap.SugaredLogger {
+	once.Do(func() {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			panic("Logger can not be initialized.")
+		}
+		defer logger.Sync()
+		l = logger.Sugar()
+	})
+	return l
+}
