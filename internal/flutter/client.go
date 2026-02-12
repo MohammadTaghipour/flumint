6c31e31ba68da0b1ec4610b31258a5b29943c1da
Mohammad Taghipour <taghipourmohammad7@gmail.com>
2026-02-12 12:37:42 +0330

    ✨ flutter builder web & android

--- internal/flutter/client.go
+++ internal/flutter/client.go
@@ -1,13 +1,17 @@
 package flutter
 
 import (
+	"flumint/internal/config"
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 type Client interface {
 	GetVersion() (*VersionInfo, error)
 	RunDoctor() (string, error)
+	Build(platform, clientName string, cfg *config.ClientConfig) error
 }
 
 type CLI struct{}
@@ -33,4 +37,33 @@ func (c *CLI) GetVersion() (*VersionInfo, error) {
 	}
 
 	return parseVersion(string(out))
+}
+
+func (c *CLI) Build(platform, clientName string, cfg *config.ClientConfig) error {
+	var cmd *exec.Cmd
+
+	dartDefines := []string{}
+	for k, v := range cfg.DartDefines {
+		dartDefines = append(dartDefines, fmt.Sprintf("--dart-define=%s=%s", k, v))
+	}
+	if clientName != "" {
+		dartDefines = append(dartDefines, fmt.Sprintf("--dart-define=CLIENT=%s", clientName))
+	}
+
+	switch platform {
+	case "android":
+		args := append([]string{"build", "apk", "--release"}, dartDefines...)
+		cmd = exec.Command("flutter", args...)
+	case "web":
+		args := append([]string{"build", "web", "--release"}, dartDefines...)
+		cmd = exec.Command("flutter", args...)
+	default:
+		return fmt.Errorf("unsupported platform: %s", platform)
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Printf("Running: %s %s\n", cmd.Path, strings.Join(cmd.Args[1:], " "))
+	return cmd.Run()
 }
